79611a8ebdcb73018110d34df9532a3b1d18967f
Scott Sirowy <ssirowy@gmail.com>
2014-12-19 23:12:56 -0800

    Added a basic work route

---
+++ Crouded/WorkRoute.m
@@ -0,0 +1,13 @@
+//
+//  WorkRoute.m
+//  Crouded
+//
+//  Created by Scott Sirowy on 12/19/14.
+//  Copyright (c) 2014 App Builders Inc. All rights reserved.
+//
+
+#import "WorkRoute.h"
+
+@implementation WorkRoute
+
+@end
